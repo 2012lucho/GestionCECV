12ab064d0c13acec63949c6b8e50391a6807aef0
root <lucho.2012.tandil@gmail.com>
2015-09-30 13:07:17 -0300

    30/9 'reestructuración' 65% modificacion componente de busqueda, agregar modelos de tablas en la carpeta common

--- frontend/modules/buscador/js/buscador.js
+++ frontend/modules/buscador/js/buscador.js
@@ -2,9 +2,11 @@ const RutaWeb="http://localhost/cecv/frontend/web/index.php";
 const PeticionT="/rtodo";
 
 //n es el numero de buscador
-function InicializarBuscador(Elemento,n){
+function InicializarBuscador(Elemento,n,Titulo){
 	//Armamos el encabezado del buscador	
-	$('#'+Elemento).html("<div>Encabezado</div>");
+	$('#'+Elemento).html("<div class='bEncab"+n+" bEncab col-xs-12'><div class='Btit col-xs-8'>"+Titulo+"</div></div>");
+	//agregamos el control de buscqueda del encabezado
+	$('.bEncab'+n).append("<div class='buscalib col-xs-4'><input type='text' id='ebusca' value='Busqueda'> <a id='lbusca'><input type='button' value='Ir'></a></div>");
 	//Definimos lacaja donde mostrar los resultados
 	$('#'+Elemento).append("<div id='"+Elemento+n+"'></div>");
 	//Pedimos todos los datos y armamos la vista
@@ -26,5 +28,12 @@ function InicializarBuscador(Elemento,n){
 }
 
 $(document).ready(function () {
-	InicializarBuscador('CatalogoLib',1);	
+	InicializarBuscador('CatalogoLib',1,'Catálogo de libros');	
+	
+	$('#ebusca').on('input', function() {
+		$('#lbusca').attr('href','http://localhost/cecv/frontend/web/index.php/busca/'+encodeURIComponent($('#ebusca').val()));
+	});
+	$('#ebusca').on('click', function() {
+		$('#ebusca').val('')		
+	});
 });
