ea55f2399f34de7396cceea22dc487b92227faeb
root <lucho.2012.tandil@gmail.com>
2015-10-22 15:26:42 -0300

    22/10 Actualización control de búsqueda

--- backend/web/assets/9b83de5d/js/prestamos.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	var r ="http://localhost/cecv/backend/web/index.php";
-	InicializarBuscador('BuscaEstu',1,'Catálogo de libros','DatosUser','NombreyApellido',r,5
-						
-						);	
-	InicializarBuscador('BuscaLib',1,'Catálogo de libros','Stock','Nombre',r,5
-						);	
-});
---
+++ bcaches.sh
@@ -0,0 +1,2 @@
+rm -r /var/www/html/cecv/frontend/web/assets/*
+rm -r /var/www/html/cecv/backend/web/assets/*
