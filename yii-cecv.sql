431da773f305a9b7b02fb72e2dcda5d21ba284ea
root <lucho.2012.tandil@gmail.com>
2015-11-17 23:37:49 -0300

    17/11, actualización archivo base de datos

--- yii-cecv.sql
+++ yii-cecv.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 21-10-2015 a las 17:10:17
+-- Tiempo de generación: 17-11-2015 a las 23:35:28
 -- Versión del servidor: 5.5.43
 -- Versión de PHP: 5.6.7-1
 
@@ -41,7 +41,9 @@ CREATE TABLE IF NOT EXISTS `configuracion` (
 --
 
 INSERT INTO `configuracion` (`cod`, `valor`, `descripcion`, `control`, `unidad`, `categoria`) VALUES
-('TPrestaLibro', '7', 'períodode tiempo máximo de debolución de material', 'number', 'Días', '');
+('CantLibSel', '2', 'Cantidad de libros que se pueden seleccionar por préstamo ingresado', 'number', '', ''),
+('PrestaMulti', 'no', 'Define si se puede cargar un prestamo nuevo, cuando todavía no existe devolución', 'checkbox', '', ''),
+('TPrestaLibro', '7', 'Periodo de tiempo máximo de devolución de material ', 'number', 'Días', '');
 
 -- --------------------------------------------------------
 
@@ -102,16 +104,37 @@ CREATE TABLE IF NOT EXISTS `Prestamos` (
   PRIMARY KEY (`idPresta`),
   KEY `idUser` (`idUser`),
   KEY `IdStock` (`IdStock`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
 
 --
 -- Volcado de datos para la tabla `Prestamos`
 --
 
 INSERT INTO `Prestamos` (`idPresta`, `idUser`, `IdStock`, `FechaPresta`, `FechaDebT`, `FechaDeb`) VALUES
-(1, 1, 10, '2015-10-13', '2015-10-20', '0000-00-00'),
-(2, 1, 10, '2015-10-05', '2015-10-19', '0000-00-00'),
-(3, 2, 6, '2015-10-06', '2015-10-28', '0000-00-00');
+(1, 1, 10, '2015-10-13', '2015-10-20', '2015-11-16'),
+(14, 2, 2, '2015-11-16', '2015-11-23', '2015-11-16'),
+(15, 2, 2, '2015-11-16', '2015-11-23', '2015-11-17'),
+(16, 2, 2, '2015-11-16', '2015-11-23', '2015-11-16'),
+(17, 2, 2, '2015-11-16', '2015-11-23', '2015-11-16'),
+(18, 2, 5, '2015-11-16', '2015-11-23', '2015-11-17'),
+(19, 1, 3, '2015-11-16', '2015-11-23', '2015-11-17'),
+(20, 2, 6, '2015-11-16', '2015-11-23', '2015-11-17'),
+(21, 1, 3, '2015-11-16', '2015-11-23', '2015-11-17'),
+(22, 1, 2, '2015-11-16', '2015-11-23', '2015-11-17'),
+(23, 1, 1, '2015-11-16', '2015-11-23', '2015-11-17'),
+(24, 1, 2, '2015-11-16', '2015-11-23', '2015-11-17'),
+(25, 1, 2, '2015-11-16', '2015-11-23', '2015-11-17'),
+(26, 1, 1, '2015-11-16', '2015-11-23', '2015-11-17'),
+(27, 1, 1, '2015-11-17', '2015-11-24', '2015-11-17'),
+(28, 1, 2, '2015-11-17', '2015-11-24', '2015-11-17'),
+(29, 1, 1, '2015-11-17', '2015-11-24', '2015-11-17'),
+(30, 1, 2, '2015-11-17', '2015-11-24', '2015-11-17'),
+(31, 2, 1, '2015-11-17', '2015-10-24', '0000-00-00'),
+(32, 2, 2, '2015-11-17', '2015-11-24', '2015-11-17'),
+(33, 1, 1, '2015-11-17', '2015-11-24', '2015-11-17'),
+(34, 1, 2, '2015-11-17', '2015-11-24', '2015-11-17'),
+(35, 1, 1, '2015-11-17', '2015-11-24', '0000-00-00'),
+(36, 1, 2, '2015-11-17', '2015-11-24', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -157,11 +180,11 @@ CREATE TABLE IF NOT EXISTS `Stock` (
 --
 
 INSERT INTO `Stock` (`idStock`, `Codigo`, `Nombre`, `Descripcion`, `Autor`, `Cantidad`, `CantidadDisponible`) VALUES
-(1, '5566876764', 'Noriega Super Stars', 'El más exitoso libro de Análisis matemático de todos los tiempos, aquí para que lo disfrutes! :D', 'Arnold Swatchzseneger', 200, 150),
-(2, '7767676g', 'Matemática Corporativa I', 'Las oscuras garras del capitalismo nunca encontraron mejor aliado en las matemáticas aprende a robar la plusvalía como un campeón', '', 150, 10),
-(3, 'hhjh344', 'La verdad de la milanesa', 'Oculta detrás de un halo de pan rallado se encuentra la verdad, aquello que nunca podrá ser negado', '', 45, 43),
+(1, '5566876764', 'Noriega Super Stars', 'El más exitoso libro de Análisis matemático de todos los tiempos, aquí para que lo disfrutes! :D', 'Arnold Swatchzseneger', 200, 148),
+(2, '7767676g', 'Matemática Corporativa I', 'Las oscuras garras del capitalismo nunca encontraron mejor aliado en las matemáticas aprende a robar la plusvalía como un campeón', '', 149, 9),
+(3, 'hhjh344', 'La verdad de la milanesa', 'Oculta detrás de un halo de pan rallado se encuentra la verdad, aquello que nunca podrá ser negado', '', 45, 41),
 (5, 'hgbj452', 'El agua y el aceite, más allá de la poesia', 'Poesia para cuando uno se aburre y no hay nada mejor para leer', '', 3, 0),
-(6, '343455edd', 'Matemática Corporativa II', 'Economía mundial en tiempo de buitres, fino análisis matemático de las antimañas financieras de los paises del "primer mundo".', '', 200, 175),
+(6, '343455edd', 'Matemática Corporativa II', 'Economía mundial en tiempo de buitres, fino análisis matemático de las antimañas financieras de los paises del "primer mundo".', '', 200, 174),
 (7, 'shgh765', 'Revolución Productiva ', 'Allá por la época del 1700 y pico parece que se empezó a generar cambios en las formas de producir, la máquina de vapor y la generadora de humo, grandes inventos de la ciencia cuasi contemporanea', 'Stiven Sigal', 260, 245),
 (8, 'jjghgjhgjg', 'De limón y Sal Vodka y mucho más', 'Que sería de la barra sin bebidas fuertes, puedes preparar muchos cocteles con gran porcentaje etílico y hermoso sabor!', 'Cherenkov', 200, 23),
 (9, '2353453', 'El gato, universo microscópico', 'El gato está vivo y muerto al mismo tiempo! Descubrelo a través de las 1200 páginas que te ofrece este libro!', 'Schoringer', 100, 90),
