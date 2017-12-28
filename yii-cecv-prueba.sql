d10af81da64556e2526a3249808871d523af2744
usuario <lucho.2012.tandil@gmail.com>
2017-12-28 14:48:12 -0300

    actualizacion de archivo de prueba de la base de datos

--- yii-cecv-prueba.sql
+++ yii-cecv-prueba.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 09-12-2015 a las 16:20:31
--- Versión del servidor: 5.5.46-0+deb8u1
--- Versión de PHP: 5.6.14-0+deb8u1
+-- Host: localhost
+-- Generation Time: Dec 28, 2017 at 03:47 PM
+-- Server version: 5.7.17-0ubuntu1
+-- PHP Version: 7.0.15-1ubuntu4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `yii-cecv`
+-- Database: `yii-cecv`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `configuracion`
+-- Table structure for table `configuracion`
 --
 
 CREATE TABLE `configuracion` (
@@ -36,18 +38,17 @@ CREATE TABLE `configuracion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `configuracion`
+-- Dumping data for table `configuracion`
 --
 
 INSERT INTO `configuracion` (`cod`, `valor`, `descripcion`, `control`, `unidad`, `categoria`) VALUES
 ('CantLibSel', '2', 'Cantidad de libros que se pueden seleccionar por préstamo ingresado', 'number', '', ''),
-('DirWeb', '/GestionCECV', 'Directorio base del sistema (modificar solo admin)', 'textbox', '', ''),
 ('TPrestaLibro', '9', 'Periodo de tiempo máximo de devolución de material ', 'number', 'Días', '');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `DatosUser`
+-- Table structure for table `DatosUser`
 --
 
 CREATE TABLE `DatosUser` (
@@ -60,16 +61,17 @@ CREATE TABLE `DatosUser` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `DatosUser`
+-- Dumping data for table `DatosUser`
 --
 
 INSERT INTO `DatosUser` (`IdUser`, `NombreyApellido`, `DNI`, `Email`, `Telefono`, `Suspendido`) VALUES
-(2, 'Ariel Soza', '23789654', '233@ddd', '23232332', 0);
+(2, 'Ariel Soza', '23789654', '233@ddd', '23232332', 0),
+(3, 'Jazmin Rosé', '34556456', 'jazmin@gmail.com', '234 4556 4567', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `migration`
+-- Table structure for table `migration`
 --
 
 CREATE TABLE `migration` (
@@ -78,7 +80,7 @@ CREATE TABLE `migration` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `migration`
+-- Dumping data for table `migration`
 --
 
 INSERT INTO `migration` (`version`, `apply_time`) VALUES
@@ -88,36 +90,29 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Prestamos`
+-- Table structure for table `Prestamos`
 --
 
 CREATE TABLE `Prestamos` (
   `idPresta` int(10) NOT NULL,
   `idUser` int(10) NOT NULL,
   `IdStock` int(10) NOT NULL,
-  `FechaPresta` date NOT NULL,
-  `FechaDebT` date NOT NULL,
-  `FechaDeb` date NOT NULL
+  `FechaPresta` date DEFAULT NULL,
+  `FechaDebT` date DEFAULT NULL,
+  `FechaDeb` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `Prestamos`
+-- Dumping data for table `Prestamos`
 --
 
 INSERT INTO `Prestamos` (`idPresta`, `idUser`, `IdStock`, `FechaPresta`, `FechaDebT`, `FechaDeb`) VALUES
-(14, 2, 2, '2015-11-16', '2015-11-23', '2015-11-16'),
-(15, 2, 2, '2015-11-16', '2015-11-23', '2015-11-17'),
-(16, 2, 2, '2015-11-16', '2015-11-23', '2015-11-16'),
-(17, 2, 2, '2015-11-16', '2015-11-23', '2015-11-16'),
-(18, 2, 5, '2015-11-16', '2015-11-23', '2015-11-17'),
-(20, 2, 6, '2015-11-16', '2015-11-23', '2015-11-17'),
-(31, 2, 1, '2015-11-17', '2015-10-24', '2015-11-26'),
-(32, 2, 2, '2015-11-17', '2015-11-24', '2015-11-17');
+(1, 2, 1, '2017-12-28', '2018-01-06', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `rangos`
+-- Table structure for table `rangos`
 --
 
 CREATE TABLE `rangos` (
@@ -129,7 +124,7 @@ CREATE TABLE `rangos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `rangos`
+-- Dumping data for table `rangos`
 --
 
 INSERT INTO `rangos` (`rango`, `nombrerango`, `editar user`, `agregar user`, `agregar catalogo`) VALUES
@@ -138,7 +133,7 @@ INSERT INTO `rangos` (`rango`, `nombrerango`, `editar user`, `agregar user`, `ag
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Stock`
+-- Table structure for table `Stock`
 --
 
 CREATE TABLE `Stock` (
@@ -152,15 +147,15 @@ CREATE TABLE `Stock` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `Stock`
+-- Dumping data for table `Stock`
 --
 
 INSERT INTO `Stock` (`idStock`, `Codigo`, `Nombre`, `Descripcion`, `Autor`, `Cantidad`, `CantidadDisponible`) VALUES
-(1, '5566876764', 'Noriega Super Stars', 'El más exitoso libro de Análisis matemático de todos los tiempos, aquí para que lo disfrutes! :D', 'Arnold Swatchzseneger', 200, 148),
+(1, '5566876764', 'Noriega Super Stars', 'El más exitoso libro de Análisis matemático de todos los tiempos, aquí para que lo disfrutes! :D', 'Arnold Swatchzseneger', 202, 145),
 (2, '7767676g', 'Matemática Corporativa I', 'Las oscuras garras del capitalismo nunca encontraron mejor aliado en las matemáticas aprende a robar la plusvalía como un campeón', '', 149, 8),
 (3, 'hhjh344', 'La verdad de la milanesa', 'Oculta detrás de un halo de pan rallado se encuentra la verdad, aquello que nunca podrá ser negado', '', 45, 41),
 (5, 'hgbj452', 'El agua y el aceite, más allá de la poesia', 'Poesia para cuando uno se aburre y no hay nada mejor para leer', '', 3, 0),
-(6, '343455edd', 'Matemática Corporativa II', 'Economía mundial en tiempo de buitres, fino análisis matemático de las antimañas financieras de los paises del "primer mundo".', '', 200, 174),
+(6, '343455edd', 'Matemática Corporativa II', 'Economía mundial en tiempo de buitres, fino análisis matemático de las antimañas financieras de los paises del \"primer mundo\".', '', 200, 174),
 (7, 'shgh765', 'Revolución Productiva ', 'Allá por la época del 1700 y pico parece que se empezó a generar cambios en las formas de producir, la máquina de vapor y la generadora de humo, grandes inventos de la ciencia cuasi contemporanea', 'Stiven Sigal', 260, 245),
 (8, 'jjghgjhgjg', 'De limón y Sal Vodka y mucho más', 'Que sería de la barra sin bebidas fuertes, puedes preparar muchos cocteles con gran porcentaje etílico y hermoso sabor!', 'Cherenkov', 200, 23),
 (9, '2353453', 'El gato, universo microscópico', 'El gato está vivo y muerto al mismo tiempo! Descubrelo a través de las 1200 páginas que te ofrece este libro!', 'Schoringer', 100, 90),
@@ -172,7 +167,7 @@ INSERT INTO `Stock` (`idStock`, `Codigo`, `Nombre`, `Descripcion`, `Autor`, `Can
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -189,37 +184,37 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Volcado de datos para la tabla `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `rango`, `status`, `created_at`, `updated_at`) VALUES
-(2, 'lucho', 'o7m2YowYjj-1MYv_Kl0YdNE5J_UsGu0p', '$2y$13$6Wz7jDuP3Y.Deuo1YV1ELOrNY6/zpUikNcfwAztWHC28iMmyOMTum', NULL, 'lucho@gmaill.com', 0, 10, 1441812643, 1441812643),
-(18, 'operador', 'fFhgzh7m5Il0mlzvKSZ3RES6vGiIrwOK', '$2y$13$hASy1x5Nngv36f1Z7h0G0uYGXNYlW.Hu73Q2l1TJRp5RkId76qmUO', NULL, 'aasasas@cbcb.m', 1, 10, 1448859825, 1448859825);
+(1, 'admin', 'HvGxeeGtLOoFgeCwYVvJn_l7CPoRKyGS', '$2y$13$X8OGSVZcDSfht42dGscKFOk1v8jOiCX6UO1SJUxVcxqsvk15Yy5UO', NULL, 'admin@coodesoft.com.ar', 0, 10, 1514470876, 1514470876),
+(2, 'operador', 'iRWkqsj7V0XIz2ybdcDYTj4kInZwjijR', '$2y$13$a/hCxT.b9bbaVZPBhEYrse.RE9IUgkxkDd/utiINZuBBmndUAKjn2', NULL, 'operador@coodesoft.com.ar', 1, 10, 1514477523, 1514477523);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `configuracion`
+-- Indexes for table `configuracion`
 --
 ALTER TABLE `configuracion`
   ADD PRIMARY KEY (`cod`);
 
 --
--- Indices de la tabla `DatosUser`
+-- Indexes for table `DatosUser`
 --
 ALTER TABLE `DatosUser`
   ADD PRIMARY KEY (`IdUser`);
 
 --
--- Indices de la tabla `migration`
+-- Indexes for table `migration`
 --
 ALTER TABLE `migration`
   ADD PRIMARY KEY (`version`);
 
 --
--- Indices de la tabla `Prestamos`
+-- Indexes for table `Prestamos`
 --
 ALTER TABLE `Prestamos`
   ADD PRIMARY KEY (`idPresta`),
@@ -227,19 +222,19 @@ ALTER TABLE `Prestamos`
   ADD KEY `IdStock` (`IdStock`);
 
 --
--- Indices de la tabla `rangos`
+-- Indexes for table `rangos`
 --
 ALTER TABLE `rangos`
   ADD PRIMARY KEY (`rango`);
 
 --
--- Indices de la tabla `Stock`
+-- Indexes for table `Stock`
 --
 ALTER TABLE `Stock`
   ADD PRIMARY KEY (`idStock`);
 
 --
--- Indices de la tabla `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
@@ -248,39 +243,40 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `DatosUser`
+-- AUTO_INCREMENT for table `DatosUser`
 --
 ALTER TABLE `DatosUser`
-  MODIFY `IdUser` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `IdUser` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT de la tabla `Prestamos`
+-- AUTO_INCREMENT for table `Prestamos`
 --
 ALTER TABLE `Prestamos`
-  MODIFY `idPresta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `idPresta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT de la tabla `Stock`
+-- AUTO_INCREMENT for table `Stock`
 --
 ALTER TABLE `Stock`
   MODIFY `idStock` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
--- AUTO_INCREMENT de la tabla `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `Prestamos`
+-- Constraints for table `Prestamos`
 --
 ALTER TABLE `Prestamos`
-  ADD CONSTRAINT `Prestamos_ibfk_4` FOREIGN KEY (`IdStock`) REFERENCES `Stock` (`idStock`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Prestamos_ibfk_3` FOREIGN KEY (`idUser`) REFERENCES `DatosUser` (`IdUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Prestamos_ibfk_3` FOREIGN KEY (`idUser`) REFERENCES `DatosUser` (`IdUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Prestamos_ibfk_4` FOREIGN KEY (`IdStock`) REFERENCES `Stock` (`idStock`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
